6fb20770930b077dfc5f024577bb6af0dc1922e7
Morten Johansson <morten1.kodehode@gmail.com>
2022-09-15 13:22:31 +0200

    started renderChar

---
+++ script.js
@@ -0,0 +1,29 @@
+const hero = {
+  elementId: "hero",
+  name: "Wizard",
+  avatar: "assets/wizard.png",
+  halth: 60,
+  diceRoll: 6,
+};
+
+const monster = {
+  elementId: "monster",
+  name: "Orc",
+  avatar: "assets/orc.png",
+  health: 10,
+  diceRoll: 4,
+};
+
+function renderCharacter(data) {
+  document.getElementById(data.elementId).innerHTML = `
+  <div class="character-card">
+  <h4 class="name">${data.name}</h4>
+  <img class="avatar" src="${data.avatar}" />
+  <p class="health">health: <b> ${data.health} </b></p>
+  <div class="dice-container"><div class="dice">${data.diceRoll}</div></div>
+  </div>
+  `;
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
