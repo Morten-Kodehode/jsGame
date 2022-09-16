3c658544074c6df02ccbbef8d63e47a2e6e221ec
Morten Johansson <morten1.kodehode@gmail.com>
2022-09-16 12:39:50 +0200

    some refactoring. continue work on dice

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const getDiceRollArray = (diceCount) => {
+  return new Array(diceCount)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * 6) + 1);
+};
+
+const getDiceHtml = (diceCount) => {
+  return getDiceRollArray(diceCount)
+    .map((num) => `<div class="dice">${num}</div>`)
+    .join("");
+};
+
 const hero = {
   elementId: "hero",
   name: "Wizard",
   avatar: "assets/wizard.png",
   halth: 60,
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
 };
 
 const monster = {
@@ -11,19 +24,25 @@ const monster = {
   name: "Orc",
   avatar: "assets/orc.png",
   health: 10,
-  diceRoll: 4,
+  diceRoll: [2],
+  diceCount: 1,
 };
 
-function renderCharacter(data) {
-  document.getElementById(data.elementId).innerHTML = `
+const renderCharacter = (data) => {
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+  const diceHtml = getDiceHtml(diceCount);
+
+  document.getElementById(elementId).innerHTML = `
   <div class="character-card">
-  <h4 class="name">${data.name}</h4>
-  <img class="avatar" src="${data.avatar}" />
-  <p class="health">health: <b> ${data.health} </b></p>
-  <div class="dice-container"><div class="dice">${data.diceRoll}</div></div>
+    <h4 class="name">${name}</h4>
+    <img class="avatar" src="${avatar}" />
+    <p class="health">health: <b> ${health} </b></p>
+    <div class="dice-container">
+      ${diceHtml}
+      </div>
   </div>
   `;
-}
+};
 
 renderCharacter(hero);
 renderCharacter(monster);
