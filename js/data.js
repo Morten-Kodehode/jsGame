0b3e038f366fa525232cba61b73486d05f55f342
Morten Johansson <morten1.kodehode@gmail.com>
2022-09-19 14:43:23 +0200

    continued work on game

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const characterData = {
+  hero: {
+    name: "Wizard",
+    avatar: "assets/wizard.png",
+    health: 60,
+    diceCount: 3,
+  },
+
+  monster: {
+    name: "Orc",
+    avatar: "assets/orc.png",
+    health: 10,
+    diceCount: 1,
+  },
+};
+
+export default characterData;
